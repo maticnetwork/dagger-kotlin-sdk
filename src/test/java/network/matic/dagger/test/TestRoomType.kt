9ddd19eb9545fdbdbbc51243165b9f4c6c31b0fd
Abhriya <roy.abhriya@gmail.com>
2020-01-19 10:56:15 +0530

    Test room type

---
+++ src/test/java/network/matic/dagger/test/TestRoomType.kt
@@ -0,0 +1,29 @@
+package network.matic.dagger.test
+
+import network.matic.dagger.RoomType
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class TestRoomType {
+
+    private lateinit var latestRoomType  : RoomType
+    private lateinit var confirmedRoomType : RoomType
+
+    @Before
+    fun setup(){
+        latestRoomType = RoomType.LATEST
+        confirmedRoomType = RoomType.CONFIRMED
+    }
+
+    @Test
+    fun `should return room on toString call success`(){
+        assertEquals("latest", latestRoomType.toString())
+        assertEquals("confirmed", confirmedRoomType.toString())
+    }
+
+    @Test fun `should return room on getRoom call success`(){
+        assertEquals("latest", latestRoomType.toString())
+        assertEquals("confirmed", confirmedRoomType.toString())
+    }
+}
